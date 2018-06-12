f91c3acfe90112e7bf0f2fc1af40ca18966ebc19
David Weiss <davidweiss@belbit.club>
2018-06-12 18:28:16 +0000

    add semantic UI

--- belbit-app/src/App.js
+++ belbit-app/src/App.js
@@ -1,21 +1,43 @@
 import React, { Component } from 'react';
+import { Button, Container, Header, Image, List, Icon} from 'semantic-ui-react';
 //import logo from './logo.svg';
-import './App.css';
 var logo = 'http://logok.org/wp-content/uploads/2016/10/Bitcoin-Logo-640x480.png'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Bienvenus chez Belbit</h1>
-        </header>
-        <p className="App-intro">
-	    ‘Quelques potes sont partis faire du kite et payaient leur bieres avec un pot commun. Les dettes se reglaient en Bitcoin et le reste est historique...’
+	<Container>
+	      <List bulleted horizontal>
+	        <List.Item as='a'>Wallet</List.Item>
+	        <List.Item as='a'>Moi</List.Item>
+	        <List.Item as='a'>La Team</List.Item>
+	      </List>
+	    <Container textAlign='center'>
+          <Header as='h1'>
+            <Image src={logo} />
+              Belbit
+          </Header>
+	    </Container>
+          <Container> 
+	    <div>
+	    <Container style={{fontStyle: 'oblique'}}>
+	    ‘Quelques potes sont partis au kite. Les dettes du pot commun des pots se reglaient en Bitcoin. Depuis cet investissement paye les bieres! ...’
+	    </Container>
+	    <Container>
 	    Rejoins notre coopperative et achete des bitcoin avec ta banque de Belgique!
-        </p>
-      </div>
+	    </Container>
+	    </div>
+        </Container>
+	    <Container textAlign='center' style={{marginTop:'15px'}}>
+
+	    <Button animated>
+	          <Button.Content visible>Rejoindre liste d attente</Button.Content>
+	          <Button.Content hidden>
+	            <Icon name='right arrow' />
+	          </Button.Content>
+	        </Button>
+	    </Container>
+	    </Container>
     );
   }
 }
